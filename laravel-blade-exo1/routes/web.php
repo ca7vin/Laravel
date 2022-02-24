5ffcd1cade1a7009b8c2f875a679905c1a0ef35c
Calvin <ca7vin@gmail.com>
2022-02-24 09:42:10 +0100

    Exo blade 2

--- laravel-blade-exo1/routes/web.php
+++ laravel-blade-exo1/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pages/about', function () {
+    return view('pages/about');
+});
